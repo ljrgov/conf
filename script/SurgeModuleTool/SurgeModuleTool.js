f4a5c842593d47733af3b5e53bcf21fc9aac37b7
ljrgov <129682266+ljrgov@users.noreply.github.com>
2024-09-10 10:27:25 +0800

    Update SurgeModuleTool.js

--- script/SurgeModuleTool/SurgeModuleTool.js
+++ script/SurgeModuleTool/SurgeModuleTool.js
@@ -110,7 +110,7 @@ async function update() {
   let version
   let resp
   try {
-    const url = 'https://raw.githubusercontent.com/Script-Hub-Org/Script-Hub/main/SurgeModuleTool.js?v=' + Date.now()
+    const url = 'https://raw.githubusercontent.com/ljrgov/conf/main/script/SurgeModuleTool/SurgeModuleTool.js?v=' + Date.now()
     let req = new Request(url)
     req.method = 'GET'
     req.headers = {
@@ -332,64 +332,6 @@ async function main() {
 await main();
 
 
-// @key Think @wuhu.
-async function update() {
-  const fm = FileManager.iCloud()
-  const dict = fm.documentsDirectory()
-  // const scriptName = Script.name()
-  const scriptName = 'SurgeModuleTool'
-  let version
-  let resp
-  try {
-    const url = 'https://raw.githubusercontent.com/ljrgov/conf/main/script/SurgeModuleTool/SurgeModuleTool.js?v=' + Date.now()
-    let req = new Request(url)
-    req.method = 'GET'
-    req.headers = {
-      'Cache-Control': 'no-cache',
-      Pragma: 'no-cache',
-    }
-    resp = await req.loadString()
-
-    const regex = /let ToolVersion = "([\d.]+)"/
-    const match = resp.match(regex)
-    version = match ? match[1] : ''
-  } catch (e) {
-    console.error(e)
-  }
-
-  if (!version) {
-    let alert = new Alert()
-    alert.title = 'Surge 模块工具'
-    alert.message = '无法获取在线版本'
-    alert.addCancelAction('关闭')
-    await alert.presentAlert()
-    return
-  } else {
-    let needUpdate = version > ToolVersion
-    if (!needUpdate) {
-      let alert = new Alert()
-      alert.title = 'Surge 模块工具'
-      alert.message = `当前版本: ${ToolVersion}\n在线版本: ${version}\n无需更新`
-      alert.addDestructiveAction('强制更新')
-      alert.addCancelAction('关闭')
-      idx = await alert.presentAlert()
-      if (idx === 0) {
-        needUpdate = true
-      }
-    }
-    if (needUpdate) {
-      fm.writeString(`${dict}/${scriptName}.js`, resp)
-      console.log('更新成功: ' + version)
-      let notification = new Notification()
-      notification.title = 'Surge 模块工具 更新成功: ' + version
-      notification.subtitle = '点击通知跳转'
-      notification.sound = 'default'
-      notification.openURL = `scriptable:///open/${scriptName}`
-      notification.addAction('打开脚本', `scriptable:///open/${scriptName}`, false)
-      await notification.schedule()
-    }
-  }
-}
 
 
 
