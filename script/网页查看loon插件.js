43c9d7b2397fb9450b46f390dd453be42d75b84d
ljrgov <129682266+ljrgov@users.noreply.github.com>
2024-10-29 01:09:51 +0800

    更新 网页查看loon插件.js

--- script/网页查看loon插件.js
+++ script/网页查看loon插件.js
@@ -1,6 +1,23 @@
-// kelee_plugin_view.js
-// 将 .plugin 文件内容作为纯文本返回
-$done({ 
-    headers: { "Content-Type": "text/plain" }, 
-    body: $response.body 
-});
+// nsloon_plugin_view.js
+
+// 从请求 URL 中提取 `plugin` 参数中的实际插件链接
+let pluginUrl = new URL($request.url).searchParams.get("plugin");
+
+if (pluginUrl) {
+    $httpClient.get(pluginUrl, function (error, response, data) {
+        if (error) {
+            $notify("请求失败", "无法访问插件文件", error);
+            $done({});
+            return;
+        }
+        // 将 .plugin 文件内容返回为纯文本显示
+        $done({
+            status: 200,
+            headers: { "Content-Type": "text/plain" },
+            body: data
+        });
+    });
+} else {
+    // 如果未找到 plugin 参数
+    $done({});
+}
